115e0f667070d533009833c048d8eddf2ec09f66
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2019-09-25 15:10:29 +0100

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,1 @@
-// import {ApiFetch} from "../data/ApiFetch";
-
-const ApiFetch = require("../data/ApiFetch");
-
-const Fetch = new ApiFetch();
-const M = Fetch.getData('https://rickandmortyapi.com/api/character/')
-    .then( m => m.data.results[0].name)
-
-M;
-
+//CODE HERE
